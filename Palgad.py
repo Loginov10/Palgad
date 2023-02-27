8842d7790634fb513402da76ead85f630867124d
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-02-27 12:32:58 +0200

    Update Palgad.py

--- Palgad.py
+++ Palgad.py
@@ -2,7 +2,7 @@ from module import*
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 while True:
-    print("1 - Eemalda inimene ja palg, 2 - Lisama uus inimene ja palg, 3 - sorteerimine")
+    print("1 - Eemalda inimene ja palg, 2 - Lisama uus inimene ja palg, 3 - sorteerimine, 4 - Vordsed palgad")
     a=int(input("Mida teeme: "))
     if a==1:
         udali(palgad,inimesed)
@@ -10,5 +10,7 @@ while True:
         dobav(inimesed,palgad)
     if a==3:
         Sorteerimine(inimesed,palgad)
+    if a==4:
+        Vordsed_palgad(inimesed,palgad)
     else:
         print("Viga")
